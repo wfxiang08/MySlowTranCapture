e75d2b2ea4dc3ee1403c10f070d5ec28cd878ac7
feiwang <wangfei@chunyu.me>
2015-08-05 10:46:26 +0800

    调整了log, 便于观察

--- local_addresses.cc
+++ local_addresses.cc
@@ -21,7 +21,6 @@
 
 #include <pcap.h>
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
@@ -30,7 +29,7 @@
 struct address_list {
     struct in_addr in_addr;
     struct address_list *next;
-    
+
 } address_list;
 
 int
@@ -44,46 +43,45 @@ get_addresses(void) {
         fprintf(stderr, "pcap: %s\n", errbuf);
         return 1;
     }
-    
+
     address_list_curr = &address_list;
-    
+
     for (curr = devlist; curr; curr = curr->next) {
         if (curr->flags & PCAP_IF_LOOPBACK)
             continue;
-            
+
         for (addr = curr->addresses; addr; addr = addr->next) {
             struct sockaddr *realaddr;
-            
+
             if (addr->addr)
                 realaddr = addr->addr;
             else if (addr->dstaddr)
                 realaddr = addr->dstaddr;
             else
                 continue;
-            
+
             if (realaddr->sa_family == AF_INET ||
-                    realaddr->sa_family == AF_INET6)
-            {
+                realaddr->sa_family == AF_INET6) {
                 struct sockaddr_in *sin;
-                
+
                 sin = (struct sockaddr_in *) realaddr;
-                
-                address_list_curr->next = (struct address_list*)malloc(sizeof(struct address_list));
+
+                address_list_curr->next = (struct address_list *) malloc(sizeof(struct address_list));
                 if (!address_list_curr->next)
                     abort();
-                
+
                 address_list_curr->next->in_addr = sin->sin_addr;
                 address_list_curr->next->next = NULL;
                 address_list_curr = address_list_curr->next;
-                
+
             }
-            
+
         }
-        
+
     }
-    
+
     pcap_freealldevs(devlist);
-    
+
     return 0;
 
 }
@@ -92,78 +90,78 @@ int
 parse_addresses(char addresses[]) {
     char *next, *comma;
     struct address_list *address_list_curr;
-    
+
     next = addresses;
     address_list_curr = &address_list;
-    
+
     while ((comma = strchr(next, ','))) {
         char *current;
-        
-        current = (char*)malloc((comma - next) + 1);
+
+        current = (char *) malloc((comma - next) + 1);
         if (!current)
             abort();
-        
+
         strncpy(current, next, (comma - next));
         current[comma - next] = '\0';
 
-        address_list_curr->next = (struct address_list*)malloc(sizeof(struct address_list));
+        address_list_curr->next = (struct address_list *) malloc(sizeof(struct address_list));
         if (!address_list_curr->next)
             abort();
-        
+
         address_list_curr->next->next = NULL;
-        
+
         if (!inet_aton(current, &address_list_curr->next->in_addr)) {
             free(current);
             return 1;
-            
+
         }
-        
+
         address_list_curr = address_list_curr->next;
-            
+
         free(current);
 
         next = comma + 1;
-        
+
     }
-    
-    address_list_curr->next = (struct address_list*)malloc(sizeof(struct address_list));
+
+    address_list_curr->next = (struct address_list *) malloc(sizeof(struct address_list));
     if (!address_list_curr->next)
         abort();
-    
+
     address_list_curr->next->next = NULL;
-    
+
     if (!inet_aton(next, &address_list_curr->next->in_addr))
         return 1;
-    
+
     address_list_curr = address_list_curr->next;
-            
+
     return 0;
-    
+
 }
 
 int
 free_addresses(void) {
     struct address_list *next;
-    
+
     while (address_list.next) {
         next = address_list.next->next;
         free(address_list.next);
         address_list.next = next;
-        
+
     }
-    
+
     return 0;
-    
+
 }
 
 int
 is_local_address(struct in_addr addr) {
     struct address_list *curr;
-    
+
     for (curr = address_list.next; curr; curr = curr->next)
         if (curr->in_addr.s_addr == addr.s_addr)
             return 1;
-        
+
     return 0;
-    
+
 }
